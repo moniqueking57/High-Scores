c807a76cba562624916b23becdc57359377d8635
Monique <moniqueking57@gmail.com>
2021-03-22 18:56:54 +0000

    changes

--- src/Scores.js
+++ src/Scores.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ScoreTable from "./ScoreTable.js";
+import allCountryScores from "./ScoreTable.js";
 
-const ScoreTable = (props) => {
+const Scores = (props) => {
   console.log(props);
   const { country } = props;
   return (
@@ -23,4 +23,4 @@ const ScoreTable = (props) => {
   );
 };
 
-export default ScoreTable;
+export default Scores;
